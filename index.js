ff2b06154b253bf86dbfe4264da28448ddab654e
alexanderjaramillo <alexunjm@gmail.com>
2020-04-08 19:25:38 -0500

    stablishing socket connection

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(global.port, () => {
