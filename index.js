b2940e2372fd953cd47880723acfad51d66b0a18
alexanderjaramillo <alexunjm@gmail.com>
2020-04-08 20:00:38 -0500

    send msg from back to front

--- index.js
+++ index.js
@@ -20,8 +20,21 @@ io.on('connection', (socket) => {
   });
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
+    emit(socket, msg);
+    broadcastEmit('hi to everyone');
   });
 });
+
+const emit = (socket, msg) => {
+  console.log("emit -> socket, msg", socket.id, msg)
+  socket.emit('some event', msg);
+}
+
+const broadcastEmit = (msg) => {
+  console.log("broadcastEmit -> msg", msg)
+  // io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' });
+  io.emit('some event', msg);
+}
 
 http.listen(global.port, () => {
   console.log(`Server running on http://localhost:${global.port}/`);
